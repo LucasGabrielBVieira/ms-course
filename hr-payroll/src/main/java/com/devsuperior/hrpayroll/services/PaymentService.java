e9c28598f747d1abbc763768a994f5571b785311
Lucas Gabriel Babolim Vieira <125750275+LucasGabrielBVieira@users.noreply.github.com>
2024-10-31 10:50:27 -0300

    hr-worker refeito

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,12 +1,23 @@
 package com.devsuperior.hrpayroll.services;
 
 import com.devsuperior.hrpayroll.entities.Payment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days) {
+
+        Worker worker = restTemplate.getForObject();
         return new Payment("Bob", 200.0, days);
     }
 }
